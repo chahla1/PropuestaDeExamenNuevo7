8be01f2b226017daf87bf88bd2a5b5335697d4b9
chahla1 <cbouchot@myuax.com>
2023-11-07 12:03:23 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@ int main() {
 using namespace std;
     int a, b, c;
     cout<<"Este es un programa que encuentra todos los triples pitagoricos"<<endl;
+    for( a=1; a <= 100; a++){
+        for(b=1; b <= 100; b++){
+
+        }
 
 }
