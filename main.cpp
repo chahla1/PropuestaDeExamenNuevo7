9b428360998dfe55ebd5f4a7a08dc2b4bacf8c2d
chahla1 <cbouchot@myuax.com>
2023-11-07 12:05:05 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
 using namespace std;
     int a, b, c;
     cout<<"Este es un programa que encuentra todos los triples pitagoricos"<<endl;
+    //Utilizamos bucles para iterar a traves de valores potenciales para los lados del triangulo a y b.
     for( a=1; a <= 100; a++){
         for(b=1; b <= 100; b++){
 
